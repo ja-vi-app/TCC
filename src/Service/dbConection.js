9e0b71b051b2498af91f502fdb4c4a2914a7038f
Kelly Dena <kelly.dena@ttmstech.com>
2022-09-02 21:08:52 -0300

    rm comentarios desnecessários

--- src/Service/dbConection.js
+++ src/Service/dbConection.js
@@ -1,18 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Usar dotenv para guardar as credenciais
-// const firebaseApp = initializeApp({
-//   apiKey: process.env.API_KEY,
-//   authDomain: process.env.AUTH_DOMAIN,
-//   projectId: process.env.PROJECT_ID,
-// });
-
 export const firebaseApp = initializeApp({
-  // apiKey: "AIzaSyAIKldK649ZgUp-JESNwTT-_yhd9DZkKIM",
-  // authDomain: "crud-tcc-315b0.firebaseapp.com",
-  // projectId: "crud-tcc-315b0",
-
   apiKey: "AIzaSyAIKldK649ZgUp-JESNwTT-_yhd9DZkKIM",
   authDomain: "crud-tcc-315b0.firebaseapp.com",
   projectId: "crud-tcc-315b0",
