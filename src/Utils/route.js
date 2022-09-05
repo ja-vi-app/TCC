955e55506a8f248a65ebbba4bd4e88facb4b0529
Kelly Dena <kelly.dena@ttmstech.com>
2022-09-05 20:06:12 -0300

    rm comentários e css

--- src/Utils/route.js
+++ src/Utils/route.js
@@ -2,23 +2,17 @@ import Home from "../Pages/Home";
 import Login from "../Pages/Login";
 import { URLS } from "./Constants";
 
- let routes = [
-    {
-        isPrivate: true,
-        route: URLS.home,
-        component: <Home />
-    },
-    // Para adicionar mais rotas basta colocar um novo OBJ como o de exemplo abaixo
-    // {
-    //     isPrivate: true,
-    //     route: '/sub-page',
-    //     component: <SubPage />
-    // },
-    {
-        isPrivate: false,
-        route: URLS.login,
-        component: <Login />
-    }
-]
+let routes = [
+  {
+    isPrivate: true,
+    route: URLS.home,
+    component: <Home />,
+  },
+  {
+    isPrivate: false,
+    route: URLS.login,
+    component: <Login />,
+  },
+];
 
-export default routes
+export default routes;
