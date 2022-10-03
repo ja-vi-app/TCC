48d03435f45c5a84c87f86fdcd681985a9edf92b
Kelly Dena <kelly.dena@ttmstech.com>
2022-10-03 19:52:37 -0300

    rm cores desnecessarias

--- src/assets/theme/base/colors.js
+++ src/assets/theme/base/colors.js
@@ -4,24 +4,16 @@ const colors = {
     default: "#f0f2f5",
   },
 
-  text: {
-    main: "#7b809a",
-    focus: "#7b809a",
-  },
-
   transparent: {
     main: "transparent",
   },
 
   white: {
     main: "#ffffff",
-    focus: "#ffffff",
   },
 
   black: {
     light: "#000000",
-    main: "#000000",
-    focus: "#000000",
   },
 };
 
