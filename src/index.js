ae3743d5f5f3274cb9626cb9dd3499066899b7b6
Felipe Scherer <felipescherer25@gmail.com>
2022-10-12 00:09:38 -0300

    router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 
 import "react-toastify/dist/ReactToastify.css";
@@ -8,9 +8,9 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
