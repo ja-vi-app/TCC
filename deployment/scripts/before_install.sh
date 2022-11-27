eaddd558dfb93bf7694d3973ca0bf7dc3e9266e3
Daniel <dlj.daniel@gmail.com>
2022-11-27 20:06:10 -0300

    source files configs

--- deployment/scripts/before_install.sh
+++ deployment/scripts/before_install.sh
@@ -12,11 +12,5 @@ else
   echo "$BACKEND_PID_FILE does NOT exist."
 fi
 
-python3 -m pip install --upgrade pip
-python3 -m pip install --no-cache-dir -r requirements.txt
-
-
-
-# curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-# yum -y install nodejs npm
-
+# python3 -m pip install --upgrade pip
+# python3 -m pip install --no-cache-dir -r requirements.txt
