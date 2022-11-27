ae6a5f55ef6f16e8a449219b9b5ba227da410530
Daniel <dlj.daniel@gmail.com>
2022-11-27 16:12:42 -0300

    Fixing typo

--- deployment/scripts/app_start.sh
+++ deployment/scripts/app_start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-cd /home/ec2-user/server/src
+
+# cd /home/ec2-user/
+# nohup python3 -m uvicorn main:app --reload --host 0.0.0.0 --port 8080 > app.log 2>&1 &
+# echo $! > app_pid.txt
+
+cd /home/ec2-user/frontend-server/src
 npm start
 # pm2 start npm --name "covidapp" -- start
 # pm2 startup
--- deployment/scripts/before_install.sh
+++ deployment/scripts/before_install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 cd /home/ec2-user/frontend-server
 echo "Moved to frontend-server folder"
+
+# python3 -m pip install --upgrade pip
+# python3 -m pip install --no-cache-dir -r requirements.txt
+
+
+
 # curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 # yum -y install nodejs npm
+
