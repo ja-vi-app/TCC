9c5e426ea848dbba2003b3ec5b7a8f61460a753b
Daniel <dlj.daniel@gmail.com>
2022-11-27 15:23:30 -0300

    fixing vuild stage references

--- frontend/scripts/before_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/frontend-server
-echo "Moved to frontend-server folder"
-# curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-# yum -y install nodejs npm
--- scripts/after_install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/frontend-server
-npm install
-# npm install --save react react-dom react-scripts react-particles-js
-# npm install pm2 -g
